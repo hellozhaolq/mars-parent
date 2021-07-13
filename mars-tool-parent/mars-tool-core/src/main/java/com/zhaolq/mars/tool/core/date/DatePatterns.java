2aee73268b4fb7839a37781923e2daf3919c52c1
zhaolq <hello@zhaolq.com>
2021-07-13 22:00:06 +0800

    update

---
+++ mars-tool-parent/mars-tool-core/src/main/java/com/zhaolq/mars/tool/core/date/DatePatterns.java
@@ -0,0 +1,12 @@
+package com.zhaolq.mars.tool.core.date;
+
+import cn.hutool.core.date.DatePattern;
+
+/**
+ * 日期格式化类，提供常用的日期格式化对象
+ *
+ * @author zhaolq
+ * @date 2021/7/13 21:32
+ */
+public class DatePatterns extends DatePattern {
+}

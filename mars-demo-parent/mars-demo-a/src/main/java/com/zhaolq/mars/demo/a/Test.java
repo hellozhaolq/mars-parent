d697a0260fe2334f7ce532d159844a2a2f4c4a73
zhaolq <hello@zhaolq.com>
2021-07-23 15:02:04 +0800

    update

--- mars-demo-parent/mars-demo-a/src/main/java/com/zhaolq/mars/demo/a/Test.java
+++ mars-demo-parent/mars-demo-a/src/main/java/com/zhaolq/mars/demo/a/Test.java
@@ -1,8 +1,9 @@
 package com.zhaolq.mars.demo.a;
 
-import lombok.Data;
-
-import java.math.BigDecimal;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
@@ -12,49 +13,51 @@ import java.math.BigDecimal;
  */
 public class Test {
     public static void main(String[] args) {
-        assert 1==2;
-        Goods goods = new Goods();
+        ThreadGroup group = new ThreadGroup("新建线程组1");
 
-        Producer producer = new Producer(goods);
-        Consumer consumer = new Consumer(goods);
+        MyThread myThread = new MyThread();
+        Thread thread1 = new Thread(group, myThread, "thread1");
+        Thread thread2 = new Thread(group, myThread, "thread2");
 
-        Thread producerThread = new Thread(producer);
-        Thread consumerThread = new Thread(consumer);
+        thread1.start();
+        thread2.start();
 
-        producerThread.start();
-        consumerThread.start();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("此线程组的名称：" + group.getName());
+        System.out.println("此线程组及其子组中活动线程数的估计值：" + group.activeCount());
+
+        System.out.println("将此线程组及其子组中的每个活动线程复制到指定的数组中：");
+        Thread[] arrary = new Thread[2];
+        int num = group.enumerate(arrary);
+        System.out.println("放入数组的线程数：" + num);
+
+        System.out.println("设置组的最大优先级,线程组中已经具有更高优先级的线程不受影响：");
+        group.setMaxPriority(5);
+
+        System.out.println("返回此线程组的父级：" + group.getParent());
+
+        System.out.println("中断此线程组中的所有线程：");
+        group.interrupt();
+
+        System.out.println("测试此线程组是否已被销毁：" + group.isDestroyed());
+        System.out.println("销毁此线程组及其所有子组。此线程组必须为空，表示此线程组中的所有线程都已停止：");
+        group.destroy();
+        System.out.println("测试此线程组是否已被销毁：" + group.isDestroyed());
+
     }
 }
 
-class Producer implements Runnable {
-    private Goods goods;
-
-    public Producer(Goods goods) {
-        this.goods = goods;
-    }
-
+class MyThread implements Runnable {
     @Override
     public void run() {
-        goods.setName("烧鸡");
-        goods.setPrice(new BigDecimal(66));
+        if (!Thread.currentThread().isInterrupted()) {
+            System.out.println(Thread.currentThread().getThreadGroup().getName() + " " + Thread.currentThread().getName());
+        }
+        System.out.println(Thread.currentThread().getName() + " 线程已中断 ");
     }
-}
-
-class Consumer implements Runnable {
-    private Goods goods;
-
-    public Consumer(Goods goods) {
-        this.goods = goods;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("商品名称: " + goods.getName() + "\t商品价格: " + goods.getPrice());
-    }
-}
-
-@Data
-class Goods {
-    private String name;
-    private BigDecimal price;
 }

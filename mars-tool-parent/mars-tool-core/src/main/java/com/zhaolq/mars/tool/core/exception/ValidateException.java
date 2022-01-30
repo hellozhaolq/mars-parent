48ee3f7f48c4472eed744d2b4a72ed829a8e9295
搖晃的紅酒杯 <hello@zhaolq.com>
2022-01-30 17:23:52 +0800

    Create ValidateException.java

---
+++ mars-tool-parent/mars-tool-core/src/main/java/com/zhaolq/mars/tool/core/exception/ValidateException.java
@@ -0,0 +1,36 @@
+/**
+ * 验证异常
+ *
+ * @author zwx1085453
+ * @since 2022/1/30 10:00
+ */
+public class ValidateException extends BaseException {
+    public ValidateException() {
+        super();
+    }
+
+    public ValidateException(String message) {
+        super(message);
+    }
+
+    public ValidateException(String message, Object... params) {
+        super(message, params);
+    }
+
+    public ValidateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidateException(Throwable cause) {
+        super(cause);
+    }
+
+    public ValidateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    @Override
+    protected IResultCode setExceptionResultCode() {
+        return ResultCode.UNKNOWN_ERROR;
+    }
+}

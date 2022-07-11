8bc454b9774d473a7fdd7e28619c54c23e583d8c
搖晃的紅酒杯 <hello@zhaolq.com>
2022-07-11 14:35:02 +0800

    Update R.java

--- mars-tool-parent/mars-tool-core/src/main/java/com/zhaolq/mars/tool/core/result/R.java
+++ mars-tool-parent/mars-tool-core/src/main/java/com/zhaolq/mars/tool/core/result/R.java
@@ -32,6 +32,8 @@ public final class R<T> implements Serializable {
     private String msgCh;
     @ApiModelProperty(value = "是否成功", required = true)
     private Boolean success;
+    @ApiModelProperty(value = "响应时间", required = true)
+    private String respTime = DateUtil.now();
 
     protected R(IResultCode resultCode) {
         this(resultCode.getCode(), null, resultCode.getDescEn(), resultCode.getDescCh(), resultCode.isSuccess());

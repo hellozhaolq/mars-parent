1abc37b5df2b463cb8b69309ca70213263a9ef53
搖晃的紅酒杯 <260731000@qq.com>
2023-04-24 19:48:00 +0800

    Update PoliticalStatusEntity.java

--- mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/PoliticalStatusEntity.java
+++ mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/PoliticalStatusEntity.java
@@ -1,17 +1,17 @@
 package com.zhaolq.mars.api.sys.entity;
 
+import java.io.Serializable;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-
-import java.io.Serializable;
 
 /**
  * <p>
@@ -25,18 +25,18 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("MARS_STD_POLITICAL_STATUS")
-@ApiModel(value="PoliticalStatusEntity对象", description="政治面貌")
+@Schema(description = "政治面貌")
 public class PoliticalStatusEntity extends Model<PoliticalStatusEntity> {
 
-    @ApiModelProperty(value = "代码")
+    @Schema(description = "代码")
     @TableId(value = "CODE", type = IdType.ASSIGN_ID)
     private String code;
 
-    @ApiModelProperty(value = "名称")
+    @Schema(description = "名称")
     @TableField("NAME")
     private String name;
 
-    @ApiModelProperty(value = "简称")
+    @Schema(description = "简称")
     @TableField("NAME_SHORT")
     private String nameShort;
 

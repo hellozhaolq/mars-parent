334810f89adf5ebb87aa93a553b41bb6bbc933a7
搖晃的紅酒杯 <260731000@qq.com>
2023-04-24 19:48:40 +0800

    Update RoleEntity.java

--- mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/RoleEntity.java
+++ mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/RoleEntity.java
@@ -21,8 +21,7 @@ import com.zhaolq.mars.common.valid.group.Add;
 import com.zhaolq.mars.common.valid.group.Edit;
 import com.zhaolq.mars.common.valid.group.Remove;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -39,56 +38,56 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("MARS_SYS_ROLE")
-@ApiModel(value = "RoleEntity对象", description = "角色管理")
+@Schema(description = "角色管理")
 public class RoleEntity extends Model<RoleEntity> {
 
     @JsonProperty("id")
     @NotNull(groups = {Edit.class, Remove.class}, message = "id缺失")
-    @ApiModelProperty(value = "编号")
+    @Schema(description = "编号")
     @TableId(value = "ID", type = IdType.ASSIGN_ID)
     private String id;
 
     @NotNull(groups = {Add.class}, message = "角色名称缺失")
-    @ApiModelProperty(value = "角色名称")
+    @Schema(description = "角色名称")
     @TableField("NAME")
     private String name;
 
     @NotNull(groups = {Add.class}, message = "角色代码缺失")
-    @ApiModelProperty(value = "角色代码")
+    @Schema(description = "角色代码")
     @TableField("CODE")
     private String code;
 
-    @ApiModelProperty(value = "备注")
+    @Schema(description = "备注")
     @TableField("REMARK")
     private String remark;
 
-    @ApiModelProperty(value = "创建人")
+    @Schema(description = "创建人")
     @TableField("CREATE_BY")
     private String createBy;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "创建时间")
+    @Schema(description = "创建时间")
     @TableField(value = "CREATE_TIME", fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
-    @ApiModelProperty(value = "更新人")
+    @Schema(description = "更新人")
     @TableField("LAST_UPDATE_BY")
     private String lastUpdateBy;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
-    @ApiModelProperty(value = "更新时间")
+    @Schema(description = "更新时间")
     @TableField(value = "LAST_UPDATE_TIME", fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime lastUpdateTime;
 
     @NotNull(groups = {Add.class}, message = "状态缺失")
-    @ApiModelProperty(value = "状态  0：禁用   1：正常")
+    @Schema(description = "状态  0：禁用   1：正常")
     @TableField("STATUS")
     private Byte status;
 
     @NotNull(groups = {Add.class}, message = "是否删除缺失")
-    @ApiModelProperty(value = "是否删除  -1：已删除  0：正常")
+    @Schema(description = "是否删除  -1：已删除  0：正常")
     @TableField("DEL_FLAG")
     private Byte delFlag;
 

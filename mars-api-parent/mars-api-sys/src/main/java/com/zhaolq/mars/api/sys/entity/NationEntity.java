b13312070c1e1f5ded26bf46c2f9eddb5445ca89
搖晃的紅酒杯 <260731000@qq.com>
2023-04-24 19:47:39 +0800

    Update NationEntity.java

--- mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/NationEntity.java
+++ mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/NationEntity.java
@@ -1,17 +1,17 @@
 package com.zhaolq.mars.api.sys.entity;
 
+import java.io.Serializable;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-
-import java.io.Serializable;
 
 /**
  * <p>
@@ -25,14 +25,14 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("MARS_STD_NATION")
-@ApiModel(value="NationEntity对象", description="民族 GB 3304-91")
+@Schema(description = "民族 GB 3304-91")
 public class NationEntity extends Model<NationEntity> {
 
-    @ApiModelProperty(value = "代码")
+    @Schema(description = "代码")
     @TableId(value = "CODE", type = IdType.ASSIGN_ID)
     private String code;
 
-    @ApiModelProperty(value = "名称")
+    @Schema(description = "名称")
     @TableField("NAME")
     private String name;
 

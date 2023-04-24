ec2284fdd306d371e0d756122f0d7a675a1b69c3
搖晃的紅酒杯 <260731000@qq.com>
2023-04-24 19:46:04 +0800

    Update CountryEntity.java

--- mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/CountryEntity.java
+++ mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/CountryEntity.java
@@ -2,21 +2,21 @@ package com.zhaolq.mars.api.sys.entity;
 
 import java.io.Serializable;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
-
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
 /**
  * <p>
  * 国家 ISO 3166-1
+ * https://zh.wikipedia.org/wiki/ISO_3166-1
  * </p>
  *
  * @author zhaolq
@@ -26,18 +26,18 @@ import com.baomidou.mybatisplus.extension.activerecord.Model;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("MARS_STD_COUNTRY")
-@ApiModel(value="CountryEntity对象", description="国家 ISO 3166-1")
+@Schema(description = "国家 ISO 3166-1")
 public class CountryEntity extends Model<CountryEntity> {
 
-    @ApiModelProperty(value = "代码")
+    @Schema(description = "代码")
     @TableId(value = "CODE", type = IdType.ASSIGN_ID)
     private String code;
 
-    @ApiModelProperty(value = "名称")
+    @Schema(description = "名称")
     @TableField("NAME")
     private String name;
 
-    @ApiModelProperty(value = "英文名称")
+    @Schema(description = "英文名称")
     @TableField("NAME_EN")
     private String nameEn;
 

cf57be21c1f64101a37069eb0eef1fc56285cf99
搖晃的紅酒杯 <260731000@qq.com>
2023-04-24 19:46:42 +0800

    Update DeptEntity.java

--- mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/DeptEntity.java
+++ mars-api-parent/mars-api-sys/src/main/java/com/zhaolq/mars/api/sys/entity/DeptEntity.java
@@ -1,18 +1,18 @@
 package com.zhaolq.mars.api.sys.entity;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -26,62 +26,62 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("MARS_SYS_DEPT")
-@ApiModel(value="DeptEntity对象", description="机构管理")
+@Schema(description = "机构管理")
 public class DeptEntity extends Model<DeptEntity> {
 
-    @ApiModelProperty(value = "编号")
+    @Schema(description = "编号")
     @TableId(value = "ID", type = IdType.ASSIGN_ID)
     private String id;
 
-    @ApiModelProperty(value = "机构名称")
+    @Schema(description = "机构名称")
     @TableField("NAME")
     private String name;
 
-    @ApiModelProperty(value = "机构代码")
+    @Schema(description = "机构代码")
     @TableField("CODE")
     private String code;
 
-    @ApiModelProperty(value = "备注")
+    @Schema(description = "备注")
     @TableField("REMARK")
     private String remark;
 
-    @ApiModelProperty(value = "类型【1：集团；2：学校 】")
+    @Schema(description = "类型【1：集团；2：学校 】")
     @TableField("TYPE")
     private Integer type;
 
-    @ApiModelProperty(value = "子类型")
+    @Schema(description = "子类型")
     @TableField("SUBTYPE")
     private String subtype;
 
-    @ApiModelProperty(value = "上级机构ID，一级机构为0")
+    @Schema(description = "上级机构ID，一级机构为0")
     @TableField("PARENT_ID")
     private String parentId;
 
-    @ApiModelProperty(value = "排序")
+    @Schema(description = "排序")
     @TableField("ORDER_NUM")
     private Long orderNum;
 
-    @ApiModelProperty(value = "创建人")
+    @Schema(description = "创建人")
     @TableField("CREATE_BY")
     private String createBy;
 
-    @ApiModelProperty(value = "创建时间")
+    @Schema(description = "创建时间")
     @TableField("CREATE_TIME")
     private LocalDateTime createTime;
 
-    @ApiModelProperty(value = "更新人")
+    @Schema(description = "更新人")
     @TableField("LAST_UPDATE_BY")
     private String lastUpdateBy;
 
-    @ApiModelProperty(value = "更新时间")
+    @Schema(description = "更新时间")
     @TableField("LAST_UPDATE_TIME")
     private LocalDateTime lastUpdateTime;
 
-    @ApiModelProperty(value = "状态  0：禁用   1：正常")
+    @Schema(description = "状态  0：禁用   1：正常")
     @TableField("STATUS")
     private Integer status;
 
-    @ApiModelProperty(value = "是否删除  -1：已删除  0：正常")
+    @Schema(description = "是否删除  -1：已删除  0：正常")
     @TableField("DEL_FLAG")
     private Integer delFlag;
 

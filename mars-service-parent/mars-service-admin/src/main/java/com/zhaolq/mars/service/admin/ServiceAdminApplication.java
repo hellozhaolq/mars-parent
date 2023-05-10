7bacb0c0eb05812effe499bf5f8aa11c6e29b230
搖晃的紅酒杯 <260731000@qq.com>
2023-05-10 19:20:34 +0800

    Update ServiceAdminApplication.java

--- mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/ServiceAdminApplication.java
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/ServiceAdminApplication.java
@@ -1,19 +1,17 @@
 package com.zhaolq.mars.service.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.zhaolq.mars.common.spring.application.MyApplication;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-@MapperScan({"com.zhaolq.**.mapper"})
 @ComponentScan({"com.zhaolq.*"})
 @Slf4j
 public class ServiceAdminApplication {
     public static void main(String[] args) {
-        MyApplication.run("mars", ServiceAdminApplication.class, args);
+        // MyApplication.run("mars", ServiceAdminApplication.class, args);
+        SpringApplication.run(ServiceAdminApplication.class, args);
     }
 }

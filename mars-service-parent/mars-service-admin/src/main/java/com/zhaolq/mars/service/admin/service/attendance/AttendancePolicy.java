0de94721e09d98bea06cc0bb7fb0e9374d952dec
搖晃的紅酒杯 <260731000@qq.com>
2023-05-17 14:11:33 +0800

    Create AttendancePolicy.java

---
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/service/attendance/AttendancePolicy.java
@@ -0,0 +1,25 @@
+package com.zhaolq.mars.service.admin.service.attendance;
+
+import lombok.Data;
+
+/**
+ * 考勤策略
+ *
+ * @author zhaolq
+ * @date 2023/4/7 9:35
+ * @since 1.0.0
+ */
+@Data
+public class AttendancePolicy {
+    // 日考勤边界线
+    private String boundaryLine;
+    // 弹性时间
+    private String flexiTimeStart;
+    private String flexiTimeEnd;
+    // 午休时间
+    private String lunchBreakStart;
+    private String lunchBreakEnd;
+    // 晚餐时间
+    private String dinnerTimeStart;
+    private String dinnerTimeEnd;
+}

1d039610ea18f07b092f51d55a28ce9e50e5a61b
搖晃的紅酒杯 <260731000@qq.com>
2023-05-17 14:10:01 +0800

    Create AntiLockScreenController.java

---
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/controller/AntiLockScreenController.java
@@ -0,0 +1,41 @@
+package com.zhaolq.mars.service.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zhaolq.mars.service.admin.schedule.MouseMoveSched;
+import com.zhaolq.mars.tool.core.result.R;
+import com.zhaolq.mars.tool.core.result.ResultCode;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 防锁屏
+ *
+ * @author zhaolq
+ * @date 2023/5/17 10:49:07
+ * @since 1.0.0
+ */
+@Slf4j
+@RestController
+@RequestMapping(path = "/antiLockScreen", consumes = {MediaType.ALL_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+public class AntiLockScreenController {
+    @Autowired
+    private MouseMoveSched mouseMoveSched;
+
+    @RequestMapping(value = "/turnOn/{antiLockScreenMinutes}")
+    public R<Object> turnOnAntiLockScreen(@PathVariable long antiLockScreenMinutes) throws Exception {
+        if (!(antiLockScreenMinutes > 0)) {
+            return R.failure(ResultCode.FAILURE);
+        }
+        return R.success(mouseMoveSched.turnOnAntiLockScreen(antiLockScreenMinutes));
+    }
+
+    @RequestMapping(value = "/turnOff")
+    public R<Object> turnOffAntiLockScreen() {
+        return R.success(mouseMoveSched.turnOffAntiLockScreen());
+    }
+}

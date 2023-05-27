c5b054d006dc6bd655e60a2c0379e6f6af96b795
搖晃的紅酒杯 <260731000@qq.com>
2023-05-27 17:59:57 +0800

    Create TestTaskController.java

---
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/controller/test/TestTaskController.java
@@ -0,0 +1,45 @@
+package com.zhaolq.mars.service.admin.controller.test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zhaolq.mars.service.admin.task.AsyncTask;
+import com.zhaolq.mars.tool.core.result.R;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 测试控制器
+ *
+ * @author zhaolq
+ * @date 2023/5/27 9:14:18
+ * @since 1.0.0
+ */
+@Slf4j
+@RestController
+@Tag(name = "测试模块", description = "测试模块")
+@RequestMapping(path = "/testTask", consumes = {MediaType.ALL_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+public class TestTaskController {
+
+    @Resource
+    private AsyncTask asyncTask;
+
+    @GetMapping("/testAsyncTask")
+    @Operation(summary = "测试异步任务", description = "测试异步任务")
+    public R<Object> testAsyncTask() throws ExecutionException, InterruptedException {
+        asyncTask.dealNoReturnTask();
+        Future<String> f = asyncTask.dealHaveReturnTask();
+        log.debug("main线程执行完毕");
+        log.debug(f.get());
+        return R.success(f.get());
+    }
+}

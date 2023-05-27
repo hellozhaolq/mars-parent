cb08e2a6d0a7fd798d14b4213f9fdfc04d3ccd38
搖晃的紅酒杯 <260731000@qq.com>
2023-05-27 17:57:05 +0800

    Update AntiLockScreenController.java

--- mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/controller/other/AntiLockScreenController.java
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/controller/other/AntiLockScreenController.java
@@ -1,4 +1,4 @@
-package com.zhaolq.mars.service.admin.controller;
+package com.zhaolq.mars.service.admin.controller.other;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zhaolq.mars.service.admin.schedule.MouseMoveSched;
+import com.zhaolq.mars.service.admin.task.ScheduledMouseMove;
 import com.zhaolq.mars.tool.core.result.R;
 import com.zhaolq.mars.tool.core.result.ResultCode;
 
@@ -24,18 +24,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(path = "/antiLockScreen", consumes = {MediaType.ALL_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AntiLockScreenController {
     @Autowired
-    private MouseMoveSched mouseMoveSched;
+    private ScheduledMouseMove scheduledMouseMove;
 
     @RequestMapping(value = "/turnOn/{antiLockScreenMinutes}")
     public R<Object> turnOnAntiLockScreen(@PathVariable long antiLockScreenMinutes) throws Exception {
         if (!(antiLockScreenMinutes > 0)) {
             return R.failure(ResultCode.FAILURE);
         }
-        return R.success(mouseMoveSched.turnOnAntiLockScreen(antiLockScreenMinutes));
+        return R.success(scheduledMouseMove.turnOnAntiLockScreen(antiLockScreenMinutes));
     }
 
     @RequestMapping(value = "/turnOff")
     public R<Object> turnOffAntiLockScreen() {
-        return R.success(mouseMoveSched.turnOffAntiLockScreen());
+        return R.success(scheduledMouseMove.turnOffAntiLockScreen());
     }
 }

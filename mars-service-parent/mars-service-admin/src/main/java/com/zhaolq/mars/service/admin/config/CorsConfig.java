5bce8e9d6df93873f56001b88605c68811f736a3
搖晃的紅酒杯 <260731000@qq.com>
2023-06-06 18:33:43 +0800

    Update CorsConfig.java

--- mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/config/CorsConfig.java
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/config/CorsConfig.java
@@ -33,7 +33,7 @@ public class CorsConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // 当allowCredentials设置为true时，CORS 规范不允许使用"*" ，并且从 5.3 开始，该组合被拒绝，转而使用allowedOriginPatterns 。
         // corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*.com"));
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");

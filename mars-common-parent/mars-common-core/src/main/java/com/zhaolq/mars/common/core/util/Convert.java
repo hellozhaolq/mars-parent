1d4da8154608b760d7e0958ad7bce994a5ca66e6
搖晃的紅酒杯 <260731000@qq.com>
2023-06-09 16:12:30 +0800

    Create Convert.java

---
+++ mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/util/Convert.java
@@ -0,0 +1,91 @@
+package com.zhaolq.mars.common.core.util;
+
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.zhaolq.mars.common.core.constant.CharPool;
+
+/**
+ * 转换器
+ *
+ * @author zhaolq
+ * @date 2023/6/9 15:53:47
+ */
+public class Convert {
+
+    // ---------------- 全角半角转换，来源hutool
+
+    /**
+     * 半角转全角
+     *
+     * @param input String.
+     * @return 全角字符串.
+     */
+    public static String toSBC(String input) {
+        return toSBC(input, null);
+    }
+
+    /**
+     * 半角转全角
+     *
+     * @param input String
+     * @param notConvertSet 不替换的字符集合
+     * @return 全角字符串.
+     */
+    public static String toSBC(String input, Set<Character> notConvertSet) {
+        final char[] c = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (null != notConvertSet && notConvertSet.contains(c[i])) {
+                // 跳过不替换的字符
+                continue;
+            }
+
+            if (c[i] == CharPool.SPACE) {
+                c[i] = '\u3000';
+            } else if (c[i] < '\177') {
+                c[i] = (char) (c[i] + 65248);
+            }
+        }
+        return new String(c);
+    }
+
+    /**
+     * 全角转半角
+     *
+     * @param input String.
+     * @return 半角字符串
+     */
+    public static String toDBC(String input) {
+        return toDBC(input, null);
+    }
+
+    /**
+     * 替换全角为半角
+     *
+     * @param text 文本
+     * @param notConvertSet 不替换的字符集合
+     * @return 替换后的字符
+     */
+    public static String toDBC(String text, Set<Character> notConvertSet) {
+        if (StringUtils.isBlank(text)) {
+            return text;
+        }
+        final char[] c = text.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (null != notConvertSet && notConvertSet.contains(c[i])) {
+                // 跳过不替换的字符
+                continue;
+            }
+
+            if (c[i] == '\u3000' || c[i] == '\u00a0' || c[i] == '\u2007' || c[i] == '\u202F') {
+                // \u3000是中文全角空格，\u00a0、\u2007、\u202F是不间断空格
+                c[i] = ' ';
+            } else if (c[i] > '\uFF00' && c[i] < '\uFF5F') {
+                c[i] = (char) (c[i] - 65248);
+            }
+        }
+
+        return new String(c);
+    }
+}

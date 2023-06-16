6af1d53c6c3c0605568a30b5235b3b0982e23ca9
搖晃的紅酒杯 <260731000@qq.com>
2023-06-16 17:54:14 +0800

    Update Demo001.java

--- mars-demo-parent/mars-demo-spark/src/main/java/com/zhaolq/mars/demo/spark/demo/Demo001.java
+++ mars-demo-parent/mars-demo-spark/src/main/java/com/zhaolq/mars/demo/spark/demo/Demo001.java
@@ -1,5 +1,6 @@
 package com.zhaolq.mars.demo.spark.demo;
 
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
@@ -25,13 +26,19 @@ public class Demo001 extends BaseSpark {
 
     @Override
     public void process() throws Exception {
-        Dataset<Row> userDataset = sparkSession.read().jdbc(properties.getProperty("url"), userTable, properties);
-        Dataset<Row> countryDataset = sparkSession.read().format("jdbc").options(propertiesMap).option("dbtable", countryTable).load();
+        Dataset<Row> userDataset = sparkSession.read().jdbc(properties.getProperty("url"), userTable, properties).cache();
+        Dataset<Row> countryDataset = sparkSession.read().format("jdbc").options(propertiesMap).option("dbtable", countryTable).load().cache();
 
         // 将两张表注册为临时表，进行关联查询
         userDataset.createOrReplaceTempView("v_base_user");
         countryDataset.createOrReplaceTempView("v_base_country");
         Dataset<Row> result = sparkSession.sql("select u.name as user_name, c.name as country_name from v_base_user u, v_base_country c where u.country_code = c.code");
+        // 传统的join操作会导致shuffle操作。因为两个RDD中，相同的key都需要通过网络拉取到一个节点上，由一个task进行join操作。
+        Dataset<Row> dataset = userDataset.join(countryDataset);
+        // Broadcast+map不会导致洗牌操作。先广播出去，每个Executor的内存中，都会驻留一份广播的全量数据。建议数据量比较少（几百M、或一两G）的情况下使用
+        Broadcast<?> broadcastDataset = javaSparkContext.broadcast(countryDataset);
+        // 将userDataset和广播的broadcastDataset进行手动关联
+
 
         // 将查询出的结果保存到mysql表之中
         result.write().mode(SaveMode.Overwrite).jdbc(properties.getProperty("url"), outputTable, properties);
@@ -42,6 +49,8 @@ public class Demo001 extends BaseSpark {
         result.write().mode(SaveMode.Overwrite)
                 .option("encoding", "utf-8")
                 .json(outputPath + System.currentTimeMillis());
+
+
     }
 
     @Override

e8869eb5cb92a2dcc4b2338e8321d488342dc6c1
搖晃的紅酒杯 <260731000@qq.com>
2023-08-16 14:21:55 +0800

    Create CsrfFilter.java

---
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/config/filter/CsrfFilter.java
@@ -0,0 +1,19 @@
+package com.zhaolq.mars.service.admin.config.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 跨站请求伪造过滤器
+ *
+ * @author zhaolq
+ * @date 2023/8/16 14:08:29
+ */
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class CsrfFilter {
+}

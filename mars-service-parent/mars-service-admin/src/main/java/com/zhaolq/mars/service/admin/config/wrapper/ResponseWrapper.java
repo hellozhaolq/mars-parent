a653ded701b4949c7117eb9aa0ce0d51a4fe6e6d
搖晃的紅酒杯 <260731000@qq.com>
2024-06-14 17:56:07 +0800

    Create ResponseWrapper.java

---
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/config/wrapper/ResponseWrapper.java
@@ -0,0 +1,20 @@
+package com.zhaolq.mars.service.admin.config.wrapper;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ * @Author zhaolq
+ * @Date 2024/6/14 15:37:48
+ */
+public class ResponseWrapper extends HttpServletResponseWrapper {
+    /**
+     * Constructs a response adaptor wrapping the given response.
+     *
+     * @param response The response to be wrapped
+     * @throws IllegalArgumentException if the response is null
+     */
+    public ResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+}

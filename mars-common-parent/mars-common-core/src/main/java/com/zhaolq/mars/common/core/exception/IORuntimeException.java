dc8a67d55a8be8139e3f63c7db0bc1f5fb1a6588
搖晃的紅酒杯 <260731000@qq.com>
2024-06-18 16:13:21 +0800

    Update IORuntimeException.java

--- mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/IORuntimeException.java
+++ mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/IORuntimeException.java
@@ -1,16 +1,17 @@
 package com.zhaolq.mars.common.core.exception;
 
+import com.zhaolq.mars.common.core.result.ICode;
+
 /**
- * IO运行时异常，常用于对IOException的包装
+ * IO运行时异常，其超类是RuntimeException，而非Exception，与IOException完全不同。
  *
  * @Author zhaolq
  * @Date 2022/1/30 10:00
  */
-public class IORuntimeException extends BaseException {
+public class IORuntimeException extends BaseRuntimeException {
     private static final long serialVersionUID = 1L;
 
     public IORuntimeException() {
-        super();
     }
 
     public IORuntimeException(String message) {
@@ -27,5 +28,25 @@ public class IORuntimeException extends BaseException {
 
     public IORuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public IORuntimeException(ICode code) {
+        super(code);
+    }
+
+    public IORuntimeException(String message, ICode code) {
+        super(message, code);
+    }
+
+    public IORuntimeException(String message, Throwable cause, ICode code) {
+        super(message, cause, code);
+    }
+
+    public IORuntimeException(Throwable cause, ICode code) {
+        super(cause, code);
+    }
+
+    public IORuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ICode code) {
+        super(message, cause, enableSuppression, writableStackTrace, code);
     }
 }

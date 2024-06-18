6131ec6fdec4d457ddd3741ca59e164809321612
搖晃的紅酒杯 <260731000@qq.com>
2024-06-18 16:15:25 +0800

    Update and rename IResultCode.java to ICode.java

--- mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/result/ICode.java
+++ mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/result/ICode.java
@@ -6,30 +6,25 @@ package com.zhaolq.mars.common.core.result;
  * @Author zhaolq
  * @Date 2020/11/3 20:54
  */
-public interface IResultCode {
+public interface ICode {
 
     /**
      * 获取状态码
+     *
      * @return int
      */
     int getCode();
 
     /**
-     * 获取描述信息
+     * 获取信息
      *
      * @return java.lang.String
      */
-    String getDescEn();
-
-    /**
-     * 获取描述信息
-     *
-     * @return java.lang.String
-     */
-    String getDescCh();
+    String getMsg();
 
     /**
      * 是否请求成功
+     *
      * @return boolean
      */
     Boolean isSuccess();

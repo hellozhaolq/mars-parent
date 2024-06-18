faf92525c1fb3608251fec7bcbcc5c348f08053d
搖晃的紅酒杯 <260731000@qq.com>
2024-06-18 16:13:57 +0800

    Update NoResourceException.java

--- mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/NoResourceException.java
+++ mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/NoResourceException.java
@@ -1,16 +1,17 @@
 package com.zhaolq.mars.common.core.exception;
 
+import com.zhaolq.mars.common.core.result.ICode;
+
 /**
  * 资源文件或资源不存在异常
  *
  * @Author zhaolq
  * @Date 2022/1/30 10:00
  */
-public class NoResourceException extends IORuntimeException {
+public class NoResourceException extends BaseRuntimeException {
     private static final long serialVersionUID = 1L;
 
     public NoResourceException() {
-        super();
     }
 
     public NoResourceException(String message) {
@@ -27,5 +28,25 @@ public class NoResourceException extends IORuntimeException {
 
     public NoResourceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public NoResourceException(ICode code) {
+        super(code);
+    }
+
+    public NoResourceException(String message, ICode code) {
+        super(message, code);
+    }
+
+    public NoResourceException(String message, Throwable cause, ICode code) {
+        super(message, cause, code);
+    }
+
+    public NoResourceException(Throwable cause, ICode code) {
+        super(cause, code);
+    }
+
+    public NoResourceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ICode code) {
+        super(message, cause, enableSuppression, writableStackTrace, code);
     }
 }

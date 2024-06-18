2b3abdaf71483b188b2003b2e27a1e21e8872360
搖晃的紅酒杯 <260731000@qq.com>
2024-06-18 16:14:15 +0800

    Update UtilException.java

--- mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/UtilException.java
+++ mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/UtilException.java
@@ -1,16 +1,17 @@
 package com.zhaolq.mars.common.core.exception;
 
+import com.zhaolq.mars.common.core.result.ICode;
+
 /**
  * 工具类异常
  *
  * @Author zhaolq
  * @Date 2022/1/30 10:00
  */
-public class UtilException extends BaseException {
+public class UtilException extends BaseRuntimeException {
     private static final long serialVersionUID = 1L;
 
     public UtilException() {
-        super();
     }
 
     public UtilException(String message) {
@@ -27,5 +28,25 @@ public class UtilException extends BaseException {
 
     public UtilException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public UtilException(ICode code) {
+        super(code);
+    }
+
+    public UtilException(String message, ICode code) {
+        super(message, code);
+    }
+
+    public UtilException(String message, Throwable cause, ICode code) {
+        super(message, cause, code);
+    }
+
+    public UtilException(Throwable cause, ICode code) {
+        super(cause, code);
+    }
+
+    public UtilException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ICode code) {
+        super(message, cause, enableSuppression, writableStackTrace, code);
     }
 }

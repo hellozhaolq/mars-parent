25cabc34ac5c7467076339ef6f8f62e36a450605
搖晃的紅酒杯 <260731000@qq.com>
2024-06-18 16:14:33 +0800

    Update ValidateException.java

--- mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/ValidateException.java
+++ mars-common-parent/mars-common-core/src/main/java/com/zhaolq/mars/common/core/exception/ValidateException.java
@@ -1,16 +1,17 @@
 package com.zhaolq.mars.common.core.exception;
 
+import com.zhaolq.mars.common.core.result.ICode;
+
 /**
  * 验证异常
  *
  * @Author zhaolq
  * @Date 2022/1/30 10:00
  */
-public class ValidateException extends BaseException {
+public class ValidateException extends BaseRuntimeException {
     private static final long serialVersionUID = 1L;
 
     public ValidateException() {
-        super();
     }
 
     public ValidateException(String message) {
@@ -27,5 +28,25 @@ public class ValidateException extends BaseException {
 
     public ValidateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public ValidateException(ICode code) {
+        super(code);
+    }
+
+    public ValidateException(String message, ICode code) {
+        super(message, code);
+    }
+
+    public ValidateException(String message, Throwable cause, ICode code) {
+        super(message, cause, code);
+    }
+
+    public ValidateException(Throwable cause, ICode code) {
+        super(cause, code);
+    }
+
+    public ValidateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ICode code) {
+        super(message, cause, enableSuppression, writableStackTrace, code);
     }
 }

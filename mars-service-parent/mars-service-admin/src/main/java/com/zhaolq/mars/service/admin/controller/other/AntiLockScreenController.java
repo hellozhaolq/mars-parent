d0cdc1a3579a175a8099cea83e6485c3ee290e17
搖晃的紅酒杯 <260731000@qq.com>
2024-06-18 16:18:26 +0800

    Update AntiLockScreenController.java

--- mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/controller/other/AntiLockScreenController.java
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/controller/other/AntiLockScreenController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zhaolq.mars.service.admin.task.ScheduledMouseMove;
 import com.zhaolq.mars.common.core.result.R;
-import com.zhaolq.mars.common.core.result.ResultCode;
+import com.zhaolq.mars.common.core.result.ErrorCode;
+import com.zhaolq.mars.service.admin.task.ScheduledMouseMove;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,7 +29,7 @@ public class AntiLockScreenController {
     @RequestMapping(value = "/turnOn/{antiLockScreenMinutes}")
     public R<Object> turnOnAntiLockScreen(@PathVariable long antiLockScreenMinutes) throws Exception {
         if (!(antiLockScreenMinutes > 0)) {
-            return R.failure(ResultCode.FAILURE);
+            return R.failure(ErrorCode.FAILURE);
         }
         return R.success(scheduledMouseMove.turnOnAntiLockScreen(antiLockScreenMinutes));
     }

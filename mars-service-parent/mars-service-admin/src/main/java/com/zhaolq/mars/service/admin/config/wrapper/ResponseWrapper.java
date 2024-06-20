709fe8c0e23a12fc3e2ed79095dce42834ce6794
搖晃的紅酒杯 <260731000@qq.com>
2024-06-20 19:30:21 +0800

    Update ResponseWrapper.java

--- mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/config/wrapper/ResponseWrapper.java
+++ mars-service-parent/mars-service-admin/src/main/java/com/zhaolq/mars/service/admin/config/wrapper/ResponseWrapper.java
@@ -2,11 +2,13 @@ package com.zhaolq.mars.service.admin.config.wrapper;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletResponseWrapper;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Author zhaolq
  * @Date 2024/6/14 15:37:48
  */
+@Slf4j
 public class ResponseWrapper extends HttpServletResponseWrapper {
     /**
      * Constructs a response adaptor wrapping the given response.
